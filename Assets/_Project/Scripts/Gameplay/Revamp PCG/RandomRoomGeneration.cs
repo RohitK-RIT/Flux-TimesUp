9eb00169ed56bde86155fa649a07ac63af8cb5d9
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2025-03-06 02:59:54 -0500

    Update RandomRoomGeneration.cs
    Player was entering the portal even though the portal was disabled

--- Assets/_Project/Scripts/Gameplay/Revamp PCG/RandomRoomGeneration.cs
+++ Assets/_Project/Scripts/Gameplay/Revamp PCG/RandomRoomGeneration.cs
@@ -12,7 +12,7 @@ namespace _Project.Scripts.Gameplay.Revamp_PCG
         [SerializeField] private BossEnemyRoom bossRoom;
         private DungeonRoom _currentRoom;
         private bool _hasInstantiatedBossRoom = false;
-        
+
         private void Start()
         {
             InitializeRoomGeneration();
@@ -21,12 +21,13 @@ namespace _Project.Scripts.Gameplay.Revamp_PCG
         private void Update()
         {
             //check if the room if cleared of enemies and the TSM is 100
-            if (_hasInstantiatedBossRoom == false && _currentRoom.CheckIfRoomIsCleared() && TimeStabilityMeter.Instance.TimeStability >= TimeStabilityMeter.Instance.InitialTimeStability)
+            if (_hasInstantiatedBossRoom == false && _currentRoom.CheckIfRoomIsCleared() &&
+                TimeStabilityMeter.Instance.TimeStability >= TimeStabilityMeter.Instance.InitialTimeStability)
             {
                 TimeStabilityMeter.Instance.PauseTimeStabilityMeter = true;
                 _currentRoom.ShowPortal();
-                
-                if(_currentRoom.CheckIfPlayerEntersPortal())
+
+                if (_currentRoom.CheckIfPlayerEntersPortal())
                 {
                     Destroy(_currentRoom.gameObject);
                     //spawn boss room
@@ -34,9 +35,9 @@ namespace _Project.Scripts.Gameplay.Revamp_PCG
                     _hasInstantiatedBossRoom = true;
                 }
             }
-            
+
             //if the player enters the portal, generate a new room
-            if (!_hasInstantiatedBossRoom && _currentRoom.CheckIfPlayerEntersPortal())
+            if (!_hasInstantiatedBossRoom && _currentRoom.CheckIfRoomIsCleared() && _currentRoom.CheckIfPlayerEntersPortal())
             {
                 InitializeRoomGeneration();
             }
@@ -45,29 +46,30 @@ namespace _Project.Scripts.Gameplay.Revamp_PCG
         private void InitializeRoomGeneration()
         {
             //if there is a current room that exists, destroy it
-            if(_currentRoom != null)
+            if (_currentRoom != null)
             {
                 Destroy(_currentRoom.gameObject);
             }
-            
+
             var randomRoomIndex = Random.Range(0, poolOfRoomPrefabs.Length);
             var spawnedRoom = InstantiateRoom(poolOfRoomPrefabs[randomRoomIndex]);
             _currentRoom = spawnedRoom;
-            
+
             //Instantiate Player in the new room at the entry point
             LevelSceneController.Instance.Player.transform.position = _currentRoom.EntryPoint.transform.position;
-            
+
             //TODO: all enemies killed => spawn loot
         }
+
         private DungeonRoom InstantiateRoom(DungeonRoom roomToSpawn)
         {
             return Instantiate(roomToSpawn, transform.position, Quaternion.identity, transform);
         }
-        
+
         private void InstantiateBossRoom(BossEnemyRoom bossRoomToSpawn)
         {
             var bossRoomInstance = Instantiate(bossRoomToSpawn, transform.position, Quaternion.identity, transform);
-            
+
             //Instantiate Player in the boss room at the entry point
             LevelSceneController.Instance.Player.transform.position = bossRoomInstance.EntryPoint.transform.position;
         }
