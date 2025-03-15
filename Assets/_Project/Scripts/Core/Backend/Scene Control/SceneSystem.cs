c1a93f2a5175615d324fd04b3c8d55a282a926f6
RohitK-RIT <145331737+RohitK-RIT@users.noreply.github.com>
2025-03-14 21:43:56 -0400

    Removed unecessary files
    Removed the fusion library, and updated the scripts that used them.
    And some OutdoorScene.unity scene file and ThirdPersonShooter empty folder

--- Assets/_Project/Scripts/Core/Backend/Scene Control/SceneSystem.cs
+++ Assets/_Project/Scripts/Core/Backend/Scene Control/SceneSystem.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using Fusion;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -11,7 +10,6 @@ namespace _Project.Scripts.Core.Backend.Scene_Control
     public class SceneSystem : BaseSystem<SceneSystem>
     {
         protected override bool IsPersistent => true;
-        internal NetworkSceneManagerDefault NetworkSceneManager { get; private set; }
 
         /// <summary>
         /// Name of the loading scene.
@@ -30,7 +28,6 @@ namespace _Project.Scripts.Core.Backend.Scene_Control
             // Create a new gameobject for the network scene manager.
             var sceneManagerGameobject = new GameObject("Network Scene Manager");
             sceneManagerGameobject.transform.SetParent(transform);
-            NetworkSceneManager = sceneManagerGameobject.AddComponent<NetworkSceneManagerDefault>();
         }
 
         /// <summary>
@@ -69,39 +66,6 @@ namespace _Project.Scripts.Core.Backend.Scene_Control
 
             // Else, wait until the requested scene is loaded.
             yield return sceneOp;
-        }
-
-        /// <summary>
-        /// Coroutine for loading a scene.
-        /// </summary>
-        /// <param name="request">struct describing scene detail to be loaded</param>
-        public async void LoadNetworkScene(SceneLoadRequest request)
-        {
-            if (!NetworkSystem.Instance.Runner)
-            {
-                Debug.LogError("Trying to load a scene without a runner");
-                return;
-            }
-
-            // If the current scene is not the authority, return.
-            if (!NetworkSystem.Instance.Runner.IsSceneAuthority)
-                return;
-
-            // Load the loading scene first.
-            var sceneOp = NetworkSystem.Instance.Runner.LoadScene(loadingSceneName, LoadSceneMode.Additive);
-            await sceneOp;
-
-            // Load the requested scene. If the scene is not found, log an error and return.
-            sceneOp = NetworkSystem.Instance.Runner.LoadScene(request.SceneName, request.Mode);
-
-            if (!sceneOp.IsValid)
-            {
-                Debug.LogError($"Failed to load scene {request.SceneName}");
-                return;
-            }
-
-            // Else, wait until the requested scene is loaded.
-            await sceneOp;
         }
     }
 }
