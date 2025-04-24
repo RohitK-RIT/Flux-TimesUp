2e61eeb94d019820e9d5d2c2c10d218371f767d1
Heena Thadani <heenathadani26@gmail.com>
2025-04-24 00:33:27 -0400

    updated ui for other screens, resolved boss room portal text bug

--- Assets/_Project/Scripts/UI/PlayerHUD.cs
+++ Assets/_Project/Scripts/UI/PlayerHUD.cs
@@ -7,9 +7,11 @@ using _Project.Scripts.Core.Player_Controllers;
 using _Project.Scripts.Core.Weapons;
 using _Project.Scripts.Core.Weapons.Abilities;
 using _Project.Scripts.Core.Weapons.Ranged;
+using _Project.Scripts.Gameplay.Revamp_PCG;
 using _Project.Scripts.Gameplay.Time_Stability_Meter;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace _Project.Scripts.UI
@@ -63,6 +65,7 @@ namespace _Project.Scripts.UI
         // References to the player controller
         [SerializeField] public LocalPlayerController player;
         private RoomWaveController _roomWaveController;
+        [SerializeField] private RandomRoomGeneration randomRoomGeneration;
         
         private void Start()
         {
@@ -109,6 +112,11 @@ namespace _Project.Scripts.UI
         // Updates the number of enemies remaining in the room
         private void UpdateEnemiesRemaining()
         {
+            if (randomRoomGeneration.hasInstantiatedBossRoom)
+            {
+                enemiesRemaining.gameObject.SetActive(false);
+                return;
+            }
             _roomWaveController = FindObjectOfType<RoomWaveController>();
             if (_roomWaveController == null) return;
             var enemiesCount = 0;
@@ -295,6 +303,7 @@ namespace _Project.Scripts.UI
             timeStabilityBar.maxValue = TimeStabilityMeter.Instance.TotalTimeStability;
             tmsValueText.text = timeStabilityBar.value + " / " + timeStabilityBar.maxValue;
             tsmFill.color = tsmGradient.Evaluate(timeStabilityBar.normalizedValue);
+            
             /*animator.SetBool(IsBlinking, false);
             if (timeStabilityBar.value < 50)
             {
