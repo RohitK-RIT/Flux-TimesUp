6799e48151657fc48462ca1f7eec4bef37c55cf7
Roosa Mozoomdar <rm7132@g.rit.edu>
2025-07-19 23:35:08 -0400

    CSV file is getting downloaded

---
+++ Assets/_Project/Scripts/Core/Enemy/GroupEnemyBehavior/DataCollection.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class DataCollection : MonoBehaviour
+{
+    public static DataCollection Instance;
+
+    private string logPath;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            logPath = Path.Combine(Application.persistentDataPath, $"session_{System.DateTime.Now:yyyyMMdd_HHmmss}.csv");
+            File.WriteAllText(logPath, "Timestamp,Category,Event,Value,Extra\n"); // CSV header
+            Debug.Log(Application.persistentDataPath);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Log(string category, string eventName, float value = 0f, string extra = "")
+    {
+        string time = Time.time.ToString("F2");
+        string line = $"{time},{category},{eventName},{value},{extra}";
+        File.AppendAllText(logPath, line + "\n");
+    }
+}
