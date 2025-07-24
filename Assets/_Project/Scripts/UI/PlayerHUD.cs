5463143835551035751b5e07d87eb96feb31a7a9
Heena Thadani <heenathadani26@gmail.com>
2025-07-24 03:06:47 -0400

    resolved remove ui for ammo when using melee

--- Assets/_Project/Scripts/UI/PlayerHUD.cs
+++ Assets/_Project/Scripts/UI/PlayerHUD.cs
@@ -6,6 +6,7 @@ using _Project.Scripts.Core.Loadout;
 using _Project.Scripts.Core.Player_Controllers;
 using _Project.Scripts.Core.Weapons;
 using _Project.Scripts.Core.Weapons.Abilities;
+using _Project.Scripts.Core.Weapons.Melee;
 using _Project.Scripts.Core.Weapons.Ranged;
 using _Project.Scripts.Gameplay.Revamp_PCG;
 using _Project.Scripts.Gameplay.Time_Stability_Meter;
@@ -46,6 +47,7 @@ namespace _Project.Scripts.UI
         //Loadout Information
         [SerializeField] public TMP_Text currAmmo;
         [SerializeField] public TMP_Text maxAmmo;
+        [SerializeField] private GameObject ammo;
         [SerializeField] public Image primaryIconSlot;
         [SerializeField] public Image secondaryIconSlot;
         [SerializeField] public Image meleeIconSlot;
@@ -101,6 +103,11 @@ namespace _Project.Scripts.UI
 
         private void Update()
         {
+            if (player.HandController.CurrentItem is Ability or MeleeWeapon)
+            {
+                ammo.SetActive(false);
+                reloadingText.SetActive(false);
+            }
             UpdateHealthBar();
             UpdateTimeStabilityBar();
             UpdateAmmoDisplay();
@@ -207,6 +214,7 @@ namespace _Project.Scripts.UI
         {
             var currentRangedWeapon = player.HandController.CurrentItem as RangedWeapon;
             if (!currentRangedWeapon) return;
+            ammo.SetActive(true);
             currAmmo.text = currentRangedWeapon.CurrentAmmo.ToString();
             maxAmmo.text = currentRangedWeapon.MaxAmmo.ToString();
         }
